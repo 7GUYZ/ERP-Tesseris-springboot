aadcb0f303b450741c5737a5cd2a196c9c8145b1
wjdxorwns <jtj991215@naver.com>
2025-07-04 16:06:58 +0900

    2025-07-04

---
+++ src/main/java/com/chilgayz/tesseris/TesserisApplication.java
@@ -0,0 +1,13 @@
+package com.chilgayz.tesseris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesserisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesserisApplication.class, args);
+	}
+
+}
