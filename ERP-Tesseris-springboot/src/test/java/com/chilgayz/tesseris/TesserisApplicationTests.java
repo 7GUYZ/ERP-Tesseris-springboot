106ee4b50663a7d8d7ad894757444b88f40f3c36
deokkyu <han14suk@gmail.com>
2025-07-04 18:23:56 +0900

    테스트

---
+++ ERP-Tesseris-springboot/src/test/java/com/chilgayz/tesseris/TesserisApplicationTests.java
@@ -0,0 +1,14 @@
+package com.chilgayz.tesseris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesserisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	// 덕규 테스트
+}
