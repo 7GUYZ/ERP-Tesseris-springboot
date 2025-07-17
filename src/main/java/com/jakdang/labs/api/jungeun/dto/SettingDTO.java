edb89b0e46f2abcdaaca7653bbce6b2959da948e
DotoriDaegoolPower <lje3299@naver.com>
2025-07-17 14:25:11 +0900

    0717 월CM한도 조회, 수정 백엔드 완료(비밀번호 체크 백엔드까지 완료)

---
+++ src/main/java/com/jakdang/labs/api/jungeun/dto/SettingDTO.java
@@ -0,0 +1,15 @@
+package com.jakdang.labs.api.jungeun.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SettingDTO {
+    private String settingName;
+    private String settingValue;
+}
