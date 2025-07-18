b870727fb4dba4469da88d3cef1e397446648d55
wjdxorwns <jtj991215@naver.com>
2025-07-18 20:15:28 +0900

    택준새로운커밋밋

--- src/main/java/com/jakdang/labs/api/deokkyu/store/repository/HDKUserTesserisRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jakdang.labs.api.deokkyu.store.repository;
-
-import com.jakdang.labs.entity.UserTesseris;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface HDKUserTesserisRepository extends JpaRepository<UserTesseris, Integer> {
-    Optional<UserTesseris> findByUserIndex(Integer userIndex);
-} 
