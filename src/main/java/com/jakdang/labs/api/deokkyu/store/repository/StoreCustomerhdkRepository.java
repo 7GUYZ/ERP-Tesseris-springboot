bdcccd063004f0411fc9f9d792679acd8e377f8e
deokkyu <han14suk@gmail.com>
2025-07-18 18:09:51 +0900

    가맹점 신청현황 페이지

--- src/main/java/com/jakdang/labs/api/deokkyu/store/repository/StoreCustomerhdkRepository.java
+++ src/main/java/com/jakdang/labs/api/deokkyu/store/repository/StoreCustomerhdkRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StoreCustomerhdkRepository extends JpaRepository<StoreCustomer, Long> {
-    List<StoreCustomer> findByStoreStoreUserIndex(String storeStoreUserIndex);
+    List<StoreCustomer> findByStoreStoreUserIndex(String storeStoreUserIndex); // 고객 수만 반환하려고
+    List<StoreCustomer> findByStoreStoreUserIndexIn(List<Integer> storeUserIndexes); // 고객 전체 리스트트
 } 
