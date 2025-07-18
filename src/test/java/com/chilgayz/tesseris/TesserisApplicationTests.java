aae4af6fcdbae953653119c9cf89a8358ca63808
hellow <ksc931007@gmail.com>
2025-07-18 12:33:25 +0900

    20250718 ci/cd 파이프라인 구축 - 빌드오류 → 수정 - placehold 구현 - kubernetes 파일 추가 - springboot 환경변수 설정

--- src/test/java/com/chilgayz/tesseris/TesserisApplicationTests.java
+++ src/test/java/com/chilgayz/tesseris/TesserisApplicationTests.java
@@ -3,7 +3,7 @@ package com.chilgayz.tesseris;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.jakdang.labs.NewBackendApplication.class)
 class TesserisApplicationTests {
 
 	@Test
