fc1aa03ccf2d95abcab1b6c9ead936034085a896
DotoriDaegoolPower <lje3299@naver.com>
2025-07-20 21:50:13 +0900

    0720 산하 가맹점 리스트, 지도보기까지 구현(이미지는 하드코딩)

---
+++ src/main/java/com/jakdang/labs/api/jungeun/dto/UserStoreCategoryDTO.java
@@ -0,0 +1,15 @@
+package com.jakdang.labs.api.jungeun.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserStoreCategoryDTO {
+    private Integer categoryIndex;
+    private String categoryName;
+}
