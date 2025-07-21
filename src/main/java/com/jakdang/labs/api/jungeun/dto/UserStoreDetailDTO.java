97a62d18586965c4463495b2c6a7aa9795c70a1f
DotoriDaegoolPower <lje3299@naver.com>
2025-07-21 18:19:53 +0900

    0721 산하가맹점 상세보기 구현(이미지는 하드코딩) BACK

--- src/main/java/com/jakdang/labs/api/jungeun/dto/UserStoreDetailDTO.java
+++ src/main/java/com/jakdang/labs/api/jungeun/dto/UserStoreDetailDTO.java
@@ -14,8 +14,14 @@ public class UserStoreDetailDTO {
     private String storeName;
     private String storePhone;
     private String storeAddress;
+    private String storeDetailAddress;
+    private String storeSite; // 홈페이지가 있다면, url
+    private String storeMemo; // 가맹점 소개글 또는 공지글
     private String storeCategoryName;
     private Integer userCmUse; // CM 금액 (잔액 표시)
     private String storeImage; // 이미지 url
-    private Integer storeBusinessState; 
+    private Integer storeBusinessState; // 영업 상태 (-> 0이면 영업종료, 1이면 영업중, 2면 영업요일 아님, 3면 브레이크타임, 4면 영업일 미지정)
+    private String storeBusinessDate; // 장사요일들
+    private String storeBusinessHour; // 장사시간 (store_start_business_hour ~ store_end_business_hour)
+    private String storeRestHour; // 휴게시간 (store_rest_start_hour ~ store_rest_end_hour)
 }
