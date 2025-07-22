73b3e30723f675f2da3688a4a13aa175d84e68f7
jipi <okj1227@gmail.com>
2025-07-23 02:48:05 +0900

    25.07.23 User 현재 포인트 조회

---
+++ src/main/java/com/jakdang/labs/api/jihun/usercurrentpoint/controller/UserCurrentPoint.java
@@ -0,0 +1,25 @@
+package com.jakdang.labs.api.jihun.usercurrentpoint.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.jakdang.labs.api.jihun.usercurrentpoint.service.UserCurrentPointService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/api/usercurrentpoint")
+@RequiredArgsConstructor
+@Slf4j
+public class UserCurrentPoint {
+
+    private final UserCurrentPointService userCurrentPointService;
+
+    @GetMapping("/{userid}")
+    public ResponseEntity<String> GetCurrnetPoint(@PathVariable String userid) {
+        return ResponseEntity.ok(userCurrentPointService.GetCurrentPoint(userid));
+    }
+
+}
