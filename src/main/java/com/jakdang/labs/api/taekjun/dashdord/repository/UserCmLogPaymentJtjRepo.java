c1c9d31f5f3e6c09952f2eba5de78410d82837db
wjdxorwns <jtj991215@naver.com>
2025-07-22 14:51:19 +0900

    네이버 이메일 인증 api성공

---
+++ src/main/java/com/jakdang/labs/api/taekjun/dashdord/repository/UserCmLogPaymentJtjRepo.java
@@ -0,0 +1,9 @@
+package com.jakdang.labs.api.taekjun.dashdord.repository;
+
+import com.jakdang.labs.entity.UserCmLogPayment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserCmLogPaymentJtjRepo extends JpaRepository<UserCmLogPayment, Integer> {
+} 
