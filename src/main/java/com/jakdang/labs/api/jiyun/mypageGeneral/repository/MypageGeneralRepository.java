d466a585f0d86e25c4976e3917af05cdd7f4b2b2
hasol2117 <hasol211704@gmail.com>
2025-07-23 17:43:28 +0900

    0723 마이페이지 불러오기 요소 수정

--- src/main/java/com/jakdang/labs/api/jiyun/mypageGeneral/repository/MypageGeneralRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jakdang.labs.api.jiyun.mypageGeneral.repository;
-
-import com.jakdang.labs.entity.UserTesseris;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface MypageGeneralRepository extends JpaRepository<UserTesseris, Integer> {
-    Optional<UserTesseris> findByUsersId_Id(String id);
-} 
