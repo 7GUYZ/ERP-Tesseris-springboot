b5385312a138fb4934fe48d951320f0864d4611e
hasol2117 <hasol211704@gmail.com>
2025-07-23 17:43:28 +0900

    0723 마이페이지 불러오기 요소 수정

--- src/main/java/com/jakdang/labs/api/jiyun/mypageGeneral/repository/SuggestionUserkjyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jakdang.labs.api.jiyun.mypageGeneral.repository;
-
-import com.jakdang.labs.entity.SuggestionUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface SuggestionUserkjyRepository extends JpaRepository<SuggestionUser, Long> {
-    List<SuggestionUser> findBySuggestionUserIndexOrderByJoinDateDesc(Integer suggestionUserIndex);
-} 
