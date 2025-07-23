796e41795c2b2283b62c71461ca1da9a2984af11
hasol2117 <hasol211704@gmail.com>
2025-07-23 11:17:35 +0900

    0723 마이페이지 불러오기 및 추천인 목록 불러오기 제작

---
+++ src/main/java/com/jakdang/labs/api/jiyun/mypageGeneral/dto/MypageUserInfoDto.java
@@ -0,0 +1,11 @@
+package com.jakdang.labs.api.jiyun.mypageGeneral.dto;
+
+import lombok.Data;
+
+@Data
+public class MypageUserInfoDto {
+    private Long userIndex;
+    private String userId;
+    private String userName;
+    private String userPhone;
+} 
