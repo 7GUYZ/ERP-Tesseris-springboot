a61216fc7cc84d3295d638303514e02993ed71f0
deokkyu <han14suk@gmail.com>
2025-07-24 18:36:49 +0900

    0724_가맹점신청/가맹점상세정보모달

---
+++ src/main/java/com/jakdang/labs/api/deokkyu/store/repository/UserCmLogTransactionTypehdkRepo.java
@@ -0,0 +1,8 @@
+package com.jakdang.labs.api.deokkyu.store.repository;
+
+import com.jakdang.labs.entity.UserCmLogTransactionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCmLogTransactionTypehdkRepo extends JpaRepository<UserCmLogTransactionType, Integer> {
+    
+} 
