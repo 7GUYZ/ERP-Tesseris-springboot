9692a504ed869c01b68ed76932d8706212cd8e81
wjdxorwns <jtj991215@naver.com>
2025-07-24 18:31:41 +0900

    사업자, 전체유저 리스트트

---
+++ src/main/java/com/jakdang/labs/api/taekjun/businessmanlist/dto/BusinessmanListResponseDTO.java
@@ -0,0 +1,20 @@
+package com.jakdang.labs.api.taekjun.businessmanlist.dto;
+
+import lombok.Data;
+
+@Data
+public class BusinessmanListResponseDTO {
+    private Integer userIndex;
+    private String email;
+    private String userName;
+    private String userPhone;
+    private String bossEmail;
+    private String businessGradeName;
+    private String businessManDistributionFlag;
+    private String userBankName;
+    private String userBankNumber;
+    private String userBankHolder;
+    private String businessAreaName;
+    private Integer businessGradeIndex;
+    private Integer businessAreaIndex;
+} 
