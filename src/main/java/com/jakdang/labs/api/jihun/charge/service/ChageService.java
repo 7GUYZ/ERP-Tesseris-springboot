da134c8235c8058ed0c8aba02c27174ba1574314
jipi <okj1227@gmail.com>
2025-07-25 15:09:55 +0900

    25.07.25 지훈 긴급

--- src/main/java/com/jakdang/labs/api/jihun/charge/service/ChageService.java
+++ src/main/java/com/jakdang/labs/api/jihun/charge/service/ChageService.java
@@ -7,23 +7,12 @@ import java.net.http.HttpResponse;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
-
 import com.jakdang.labs.api.jihun.charge.repository.AjgTemporaryRegularDetail;
 import com.jakdang.labs.api.jihun.charge.repository.AjgTemporaryRegularMaster;
-
-import com.jakdang.labs.api.jihun.charge.repository.AjgTemporaryRegularDetail;
-import com.jakdang.labs.api.jihun.charge.repository.AjgTemporaryRegularMaster;
-
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -34,13 +23,11 @@ public class ChageService {
     private final String TOSS_SECRET_KET = "test_sk_Gv6LjeKD8aEWZO419N7k8wYxAdXy";
     private final AjgTemporaryRegularMaster ajgTemporaryRegularMaster;
     private final AjgTemporaryRegularDetail ajgTemporaryRegularDetail;
-    private final HttpServletRequest request;
 
     @Transactional
-    public ResponseEntity<Map<String, Object>> confirmPayment(Map<String, Object> data) {
-        String requestURI = ((HttpServletRequest) request).getRequestURI();
-        String referer = request.getHeader("Referer");
+    public ResponseEntity<Map<String, Object>> confirmPayment(Map<String, Object> data, String source) {
         Map<String, Object> response = new HashMap<>();
+        log.info("source : {}", source);
         try {
             log.info("결제 요청 데이터: {}", data);
             HttpRequest request = HttpRequest.newBuilder()
@@ -54,37 +41,28 @@ public class ChageService {
                     .build();
             HttpResponse<String> result = HttpClient.newHttpClient().send(request,
                     HttpResponse.BodyHandlers.ofString());
-            log.info("다음값  {}", referer);
-            log.info("여기서requestURI : {}", requestURI);
             log.info("결과값 : {}", result.body());
-            // DB처리하는곳
-            switch (getPathType(requestURI)) {
-                case "charge":
-                    chargeProcess(data);
-                    break;
-                case "mypage":
-                    mypageProcess(data);
-                    break;
-                default:
-                    break;
+            if (result.body().contains("DONE")) {
+                // DB처리하는곳
+                switch (source) {
+                    case "charge":
+                        chargeProcess(data);
+                        break;
+                    case "take":
+                        mypageProcess(data);
+                        break;
+                    default: break;
+                }
+                return ResponseEntity.status(HttpStatus.OK).body(Map.of("result", result.body()));
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("result", result.body()));
             }
-            return ResponseEntity.status(HttpStatus.OK).body(Map.of("result", result.body()));
         } catch (Exception e) {
             response.put("success", false);
             response.put("message", e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
-
-    private String getPathType(String requestURI) {
-        if (requestURI.contains("/charge")) {
-            return "charge";
-        } else if (requestURI.contains("/mypage")) {
-            return "mypage";
-        }
-        return null;
-    }
-
     private void chargeProcess(Map<String, Object> data) {
         log.info("결제 처리 시작: {}", data);
     }
