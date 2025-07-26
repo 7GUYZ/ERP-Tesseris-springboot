f1afec913baee99f283c80aa1ef933c255f6ee7f
DotoriDaegoolPower <lje3299@naver.com>
2025-07-26 23:06:35 +0900

    0726 알림 토스트 구현 완료(websocket. 월CM한도 변경만 구현함. DB에 내역 삽입은 진행 안함)

--- src/main/java/com/jakdang/labs/api/jungeun/repository/UserTesserisLjeRepo.java
+++ src/main/java/com/jakdang/labs/api/jungeun/repository/UserTesserisLjeRepo.java
@@ -1,5 +1,6 @@
 package com.jakdang.labs.api.jungeun.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,10 @@ import com.jakdang.labs.entity.UserTesseris;
 public interface UserTesserisLjeRepo extends JpaRepository<UserTesseris, Integer>{
     @Query("SELECT ut FROM UserTesseris ut WHERE ut.usersId.id = :usersId")
     Optional<UserTesseris> findByUsersId(@Param("usersId") String usersId);
+
+    @Query("SELECT CAST(ut.userIndex AS string) FROM UserTesseris ut WHERE ut.userRoleIndex = :roleIndex")
+    List<String> findUserIndexesByRole(@Param("roleIndex") int roleIndex);
+
+    @Query("SELECT CAST(a.userIndex.userIndex AS string) FROM Admin a WHERE a.adminTypeIndex.adminTypeIndex = :adminTypeIndex")
+    List<String> findAdminIndexesByType(@Param("adminTypeIndex") int adminTypeIndex);
 }
