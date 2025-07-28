090490809c0e777afd34c94957ec5891e98dbb5c
DotoriDaegoolPower <lje3299@naver.com>
2025-07-28 14:28:10 +0900

    0728 WebSocketCofing, SecurityConfig 수정 (401 문제 해결 -url 달라서)

--- src/main/java/com/jakdang/labs/config/WebSocketConfig.java
+++ src/main/java/com/jakdang/labs/config/WebSocketConfig.java
@@ -9,7 +9,6 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -28,7 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat", "/ws/notifications") // 0726 정은 추가 - 알림WebSocket
+        registry.addEndpoint("/ws/chat", "springboot/ws/notifications") // 0726 정은 추가 - 알림WebSocket
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
