3cb2761d86787124e4e4d0419fa61e4038f60813
jipi <okj1227@gmail.com>
2025-08-04 19:25:34 +0900

    25.08.04 추천인 수당지급로직수정

---
+++ src/main/java/com/jakdang/labs/api/jihun/charge/repository/AjgTemporaryRegularDetail.java
@@ -0,0 +1,11 @@
+package com.jakdang.labs.api.jihun.charge.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jakdang.labs.entity.TemporaryRegularDetail;
+
+@Repository
+public interface AjgTemporaryRegularDetail extends JpaRepository<TemporaryRegularDetail, Integer> {
+    
+} 
